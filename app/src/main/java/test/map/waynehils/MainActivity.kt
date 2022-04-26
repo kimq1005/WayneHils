15ceeb9ed8c0e367b9246da57f896eb77b6a8a72
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2022-04-26 22:36:38 +0900

    [MainActivity 구현]

--- app/src/main/java/test/map/waynehils/MainActivity.kt
+++ app/src/main/java/test/map/waynehils/MainActivity.kt
@@ -1,11 +1,64 @@
 package test.map.waynehils
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import test.map.waynehils.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mainBinding: ActivityMainBinding? = null
+    private val binding get() = mainBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        intentbtn()
+        setTransparent()
+
+
     }
+
+    //intent를 위한 함수
+    private fun intentbtn() {
+        binding.startTTVImageView.setOnClickListener {
+            val intent = Intent(this, Service_introduction_First::class.java)
+            startActivity(intent)
+        }
+    }
+
+    // Status bar를 투명하게 바꾸기 위한 함수
+    private fun setTransparent() {
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
+        }
+        if (Build.VERSION.SDK_INT >= 19) {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+        if (Build.VERSION.SDK_INT >= 21) {
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
+    // Status bar를 투명하게 바꾸기 위한 함수
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
+    }
+
 }
